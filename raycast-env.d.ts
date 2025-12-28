9cf19ace744b8c61cc8a6ae7663e62312d423207
mateusbadalotti <mateus.badalotti@gmail.com>
2025-12-28 15:07:53 -0300

    initial commit

---
+++ raycast-env.d.ts
@@ -0,0 +1,35 @@
+/// <reference types="@raycast/api">
+
+/* 🚧 🚧 🚧
+ * This file is auto-generated from the extension's manifest.
+ * Do not modify manually. Instead, update the `package.json` file.
+ * 🚧 🚧 🚧 */
+
+/* eslint-disable @typescript-eslint/ban-types */
+
+type ExtensionPreferences = {
+  /** Play sound effects through current output - Play system sounds through current output. */
+  "systemOutput": boolean
+}
+
+/** Preferences accessible in all the extension's commands */
+declare type Preferences = ExtensionPreferences
+
+declare namespace Preferences {
+  /** Preferences accessible in the `auto-switch-input` command */
+  export type AutoSwitchInput = ExtensionPreferences & {}
+  /** Preferences accessible in the `auto-switch-output` command */
+  export type AutoSwitchOutput = ExtensionPreferences & {}
+  /** Preferences accessible in the `customize-order` command */
+  export type CustomizeOrder = ExtensionPreferences & {}
+}
+
+declare namespace Arguments {
+  /** Arguments passed to the `auto-switch-input` command */
+  export type AutoSwitchInput = {}
+  /** Arguments passed to the `auto-switch-output` command */
+  export type AutoSwitchOutput = {}
+  /** Arguments passed to the `customize-order` command */
+  export type CustomizeOrder = {}
+}
+
